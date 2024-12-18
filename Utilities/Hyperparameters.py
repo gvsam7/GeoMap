fd6cd0822d0223eec6754dd54b2fb54909b3df4e
Georgios Voulgaris <gvsam7@yahoo.com>
2024-12-18 11:53:03 +0000

    added b76 in help

--- Utilities/Hyperparameters.py
+++ Utilities/Hyperparameters.py
@@ -20,7 +20,7 @@ def arguments():
     parser.add_argument("--requires-grad", default=False)
     parser.add_argument("--global-pooling", default=None)
     parser.add_argument("--class-weighting", default=False)
-    parser.add_argument("--dataset", default="b10", help="b6, b7, b10, b11")
+    parser.add_argument("--dataset", default="b10", help="b6, b7, b10, b11, b76")
     parser.add_argument("--version", default="b0", help="b1:b7")
     parser.add_argument("--architecture", default="cnn_5", help="cnn4=CNN4, cnn5=CNN5, cnn6=CNN6, cnn7=CNN7,"
                                                                 "resnet18=ResNet18, dilgabmpresnet18=DilGabMPResNet18"
