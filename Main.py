ca9fd6f77570211cd7131a7f88a3b1a6f222caf3
Georgios Voulgaris <gvsam7@yahoo.com>
2024-12-19 15:15:42 +0000

    brought it back prior fusion

--- Main.py
+++ Main.py
@@ -108,22 +108,10 @@ def main():
     print(f"Dataset is {args.dataset}")
 
     # Get labels
-    if args.dataset == 'fusion':
-        # Use the 'b10' dataset as a reference to extract class labels
-        labels = dataset['b10'].classes
-    else:
-        labels = dataset.classes
+    labels = dataset.classes
 
     num_classes = len(labels)
-    # If using fusion, handle the datasets separately
-
-    # Get targets and dataset length
-    if args.dataset == 'fusion':
-        y = dataset['b10'].targets
-    else:
-        # Regular dataset handling for non-fusion cases
-        y = dataset.targets
-    # y = dataset.targets
+    y = dataset.targets
 
     dataset_len = len(dataset)
 
@@ -305,3 +293,105 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+"""
+args = arguments()
+    wandb.init(project="PlantPollution", config=args)
+
+    # Set device
+    if torch.cuda.is_available():
+        device = torch.device(f"cuda:{args.device}")  # Can continue going on here, like cuda:1 cuda:2....etc.
+        print("Running on the GPU")
+    else:
+        device = torch.device("cpu")
+        print("Running on the CPU")
+
+    # Load data
+    dataset = database(args.dataset)
+    print(f"Dataset is {args.dataset}")
+
+    # Get labels
+    if args.dataset == 'fusion':
+        # Use the 'b10' dataset as a reference to extract class labels
+        labels = dataset['b10'].classes
+    else:
+        labels = dataset.classes
+
+    num_classes = len(labels)
+    # If using fusion, handle the datasets separately
+
+    # Get targets and dataset length
+    if args.dataset == 'fusion':
+        y = dataset['b10'].targets
+    else:
+        # Regular dataset handling for non-fusion cases
+        y = dataset.targets
+    # y = dataset.targets
+
+    dataset_len = len(dataset)
+
+    # Stratify split data
+    X_trainval, X_test, y_trainval, y_test = train_test_split(np.arange(dataset_len), y, test_size=0.2, stratify=y,
+                                                              random_state=args.random_state, shuffle=True)
+    X2 = X_trainval
+    y2 = y_trainval
+    X_train, X_val, y_train, y_val = train_test_split(X2, y2, test_size=0.2, stratify=y2,
+                                                      random_state=args.random_state, shuffle=True)
+
+    if args.dataset == 'fusion':
+        # Create subsets for each band using the same indices
+        train_ds = {
+            'b10': Subset(dataset['b10'], X_train),
+            'b11': Subset(dataset['b11'], X_train),
+            'b6': Subset(dataset['b6'], X_train),
+            'b7': Subset(dataset['b7'], X_train),
+            'b76': Subset(dataset['b76'], X_train)
+        }
+
+        val_ds = {
+            'b10': Subset(dataset['b10'], X_val),
+            'b11': Subset(dataset['b11'], X_val),
+            'b6': Subset(dataset['b6'], X_val),
+            'b7': Subset(dataset['b7'], X_val),
+            'b76': Subset(dataset['b76'], X_val)
+        }
+
+        test_ds = {
+            'b10': Subset(dataset['b10'], X_test),
+            'b11': Subset(dataset['b11'], X_test),
+            'b6': Subset(dataset['b6'], X_test),
+            'b7': Subset(dataset['b7'], X_test),
+            'b76': Subset(dataset['b76'], X_test)
+        }
+
+    else:
+        train_ds = Subset(dataset, X_train)
+        val_ds = Subset(dataset, X_val)
+        test_ds = Subset(dataset, X_test)
+        filepaths = np.array(tuple(zip(*dataset.imgs))[0])
+        train_filepaths = filepaths[X_train]
+        val_filepaths = filepaths[X_val]
+        test_filepaths = filepaths[X_test]
+
+        # Create train, validation and test datasets
+        train_dataset = DataRetrieve(
+            train_ds,
+            transforms=train_transforms(args.width, args.height, args.Augmentation),
+            augmentations=args.augmentation
+        )
+
+        val_dataset = DataRetrieve(
+            val_ds,
+            transforms=val_transforms(args.width, args.height)
+        )
+
+        test_dataset = DataRetrieve(
+            test_ds,
+            transforms=test_transforms(args.width, args.height)
+        )
+    # Create train, validation and test dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
+    prediction_loader = DataLoader(test_dataset, batch_size=args.batch_size)
+    """
