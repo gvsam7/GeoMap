1d607a9dfb8a2487346763d9914b3435693834cf
Georgios Voulgaris <gvsam7@yahoo.com>
2024-12-20 15:38:38 +0000

    brought back a step

--- models/CNN.py
+++ models/CNN.py
@@ -16,12 +16,6 @@ class CNN(nn.Module):
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
-        # Feature extractor
-        self.feature_extractor = nn.Sequential(
-            nn.Linear(512, 512),  # Assuming 512 is the number of output channels from the last conv layer
-            nn.ReLU(inplace=True)
-        )
-
         self.classifier = nn.Sequential(
             nn.Linear(in_linear, num_classes)
         )
@@ -30,11 +24,8 @@ class CNN(nn.Module):
         x = self.features(x)
         x = self.avgpool(x)
         x = x.flatten(1)
-        features = self.feature_extractor(x)  # Feature extraction
-        out = self.classifier(features)  # Classification output
-        # x = self.classifier(x)
-        # return x
-        return features, out
+        x = self.classifier(x)
+        return x
 
     def conv_layers(self, architecture):
         layers = []
